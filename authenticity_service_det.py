e09bdf965c65389b4a8d5fb13b0338aee70c37e0
ResumeGuard <chiefconcannon@gmail.com>
2025-10-24 21:34:29 -0400

    Add files via upload

--- authenticity_service_det.py
+++ authenticity_service_det.py
@@ -22,7 +22,6 @@ from datetime import date, timedelta, datetime
 from typing import Dict, Any, List, Optional, Literal, Tuple
 import re, math, calendar, hashlib
 from collections import Counter
-
 from rule_based_scorer import evaluate_resume
 
 # =========================
